692b6c566177695ab221a0b7401d2854af7ae801
powerlanguage <josh.wardle@gmail.com>
2015-11-03 09:01:46 -0800

    Start on followers.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
 from app import db
 from hashlib import md5 #using for gravatar
 
-# creating instances of the db.Column which takes field type as argument
+#No declaring this table as a model like we did for users and posts. since this is
+# an auxillary table that has no data other than the foreign keys we use lower level
+#apis to create the table without an associated model
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+  )
 
+# creating instances of the db.Column which takes field type as argument
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   nickname = db.Column(db.String(64), index=True, unique=True)
@@ -10,6 +17,36 @@ class User(db.Model):
   posts = db.relationship('Post', backref='author', lazy='dynamic')
   about_me = db.Column(db.String(140))
   last_seen = db.Column(db.DateTime)
+                              #right side entity, parent class is left
+  followed = db.relationship('User',
+                              #indicates association table used in relationship
+                              secondary=followers,
+                              #the condition that links left side (follower) to the
+                              #association table
+                              primaryjoin=(followers.c.follower_id == id),
+                              #the condition that links right side (followed) to the
+                              #association table
+                              secondaryjoin=(followers.c.followed_id == id),
+                              #defines relationship as accesssed from the right side
+                              #followed returns users following current user
+                              #followers returns users followed by current user
+                              backref=db.backref('followers', lazy='dynamic'),
+                              lazy='dynamic')
+
+  #returns an object when it succeeds or None when it fails
+  def follow(self, user):
+    if not self.is_following(user):
+      self.followed.append(user)
+      return self
+
+  #returns an object when it succeeds or None when it fails
+  def unfollow(self, user):
+    if self.is_following(user):
+      self.followed.remove(user)
+      return self
+
+  def is_following(self, user):
+    return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
   @property
   def is_authenticated(self):
